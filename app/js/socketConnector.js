ff81df4c30fe39c4c6058b1a0e590ff5aee0db1f
Aron Duby <aron.duby@gmail.com>
2015-02-19 12:35:03 -0500

    rework the socket connector to use a custom defer object to handle the race condition for socket loading before angular - fixes #166

--- app/js/socketConnector.js
+++ app/js/socketConnector.js
@@ -1,22 +1,62 @@
 // keeps angular from bootstraping until we're connected
-define(['bower_components/angular/angular.min.js'], function(){
+define([], function(){
 
 	var address = 'http://'+window.location.host+':834',
 		options = {
 			'sync disconnect on unload': true,
 			'max reconnection attempts': 5
-		},
-		bootstrapped = false;
+		}
 
 	var socket = io.connect(address, options);
 
+	// quick slap together a defer
+	var promise = {
+		okCallbacks: [],
+		koCallbacks: [],
+		status: null,
+		data: null,
+		then: function(okCallback){
+			if(this.status == 'resolved'){
+				okCallback(this.data);
+			} else {
+				this.okCallbacks.push(okCallback);
+			}
+		},
+		fail: function(koCallback){
+			if(this.status == 'rejected'){
+				koCallback(this.data);
+			} else {
+				this.koCallbacks.push(koCallback);
+			}
+		}
+	};
+
+	var defer = {
+		promise: promise,
+		resolve: function(data){
+			this.promise.status = 'resolved';
+			this.promise.data = data;
+			this.promise.okCallbacks.forEach(function(callback){
+				window.setTimeout(function(){
+					callback(data);
+				}, 0)
+			});
+		},
+		reject: function(err){
+			this.promise.status = 'rejected';
+			this.promise.data = data;
+			this.promise.koCallbacks.forEach(function(callback){
+				window.setTimeout(function(){
+					callback(err);
+				}, 0);
+			});
+		}
+	};
+
 	socket.on('connect', function(){
-		if(bootstrapped === false){
-			angular.resumeBootstrap();
-			bootstrapped = true;	
-		}		
+		defer.resolve(socket);
 	});
 
-	return socket;
+	return defer.promise;
 
 });
